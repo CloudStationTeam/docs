b052a06e856f8305f254db9d228c71d0781fa171
Lyuyang Hu <lyuyangh@uci.edu>
2020-01-05 19:31:58 -0800

    use new theme

--- source/conf.py
+++ source/conf.py
@@ -79,9 +79,7 @@ pygments_style = None
 
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'neo_rtd_theme'
-import sphinx_theme
-html_theme_path = [sphinx_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
@@ -91,10 +89,19 @@ html_theme_path = [sphinx_theme.get_html_theme_path()]
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {
-#     'stickysidebar': True,
-#     'collapsiblesidebar': True 
-# }
+html_theme_options = {
+    'canonical_url': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
